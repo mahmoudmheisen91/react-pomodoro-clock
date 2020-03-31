4a754dd4d0c33efe6f5076fe8f5b2483e655df2c
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-04-01 01:42:57 +0300

    add pomodoro comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import HeaderComp from "./components/HeaderComp";
 import FooterComp from "./components/FooterComp";
+import PomodoroComp from "./components/PomodoroComp";
 
-function App() {
-  return (
-    <div>
-      <HeaderComp text={"Learn React 2"} />
-      <FooterComp text={"Mahmoud Mheisen"} />
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <HeaderComp
+          text={"FCC: Front End Libraries - Project 5, Pomodoro Clock"}
+        />
+        <PomodoroComp />
+        <FooterComp text={"\u00A9 2020 Mahmoud Mheisen"} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/PomodoroComp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PomodoroComp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default PomodoroComp;
