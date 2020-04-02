fe11ff4662ba1786fdc759ab2eff1c6ac3d43761
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-04-02 14:40:14 +0300

    Update PomodoroComp.js

--- src/components/PomodoroComp.js
+++ src/components/PomodoroComp.js
@@ -16,6 +16,11 @@ class ControlComp extends React.Component {
       this.setState(state => ({
         counter: state.counter++
       }));
+
+      this.props.getTime({
+        type: this.props.type,
+        time: this.state.counter + 1
+      });
     }
   }
 
@@ -24,6 +29,11 @@ class ControlComp extends React.Component {
       this.setState(state => ({
         counter: state.counter--
       }));
+
+      this.props.getTime({
+        type: this.props.type,
+        time: this.state.counter - 1
+      });
     }
   }
 
@@ -31,7 +41,9 @@ class ControlComp extends React.Component {
     return (
       <div className={this.props.controlClass + " control"}>
         <div id={this.props.id}>{this.props.text}</div>
-        <div id={this.props.valueID}>{this.state.counter}</div>
+        <div onChange={this.sendValue} id={this.props.valueID}>
+          {this.state.counter}
+        </div>
         <button onClick={this.increment} id={this.props.btn1ID}>
           <i className="fas fa-arrow-alt-circle-up fa-2x icon"></i>
         </button>
@@ -43,28 +55,64 @@ class ControlComp extends React.Component {
   }
 }
 
-let DisplayComp = props => {
-  return (
-    <div className="display">
-      <div id={"timer-label"}>{"Session"}</div>
-      <div id={"time-left"}>{props.timeLeft}</div>
-      <div className="disp-btn">
-        <button id={"start_stop"}>
-          <i class="fas fa-play-circle fa-2x icon"></i>
-          {/* <i class="fas fa-pause-circle fa-2x icon"></i> */}
-        </button>
-        <button id={"reset"}>
-          <i className="fas fa-undo fa-2x icon"></i>
-        </button>
+class DisplayComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.togglePlay = this.togglePlay.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  togglePlay() {}
+
+  reset() {}
+
+  render() {
+    return (
+      <div className="display">
+        <div id={"timer-label"}>{"Session"}</div>
+        <div id={"time-left"}>{this.props.timeLeft}</div>
+        <div className="disp-btn">
+          <button onClick={this.togglePlay} id={"start_stop"}>
+            <i class="fas fa-play-circle fa-2x icon"></i>
+            {/* <i class="fas fa-pause-circle fa-2x icon"></i> */}
+          </button>
+          <button onClick={this.reset} id={"reset"}>
+            <i className="fas fa-undo fa-2x icon"></i>
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 class PomodoroComp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      minLeft: 25,
+      secLeft: 0
+    };
+
+    this.timeleftString = this.timeleftString.bind(this);
+    this.getTime = this.getTime.bind(this);
+  }
+
+  timeleftString() {
+    let min =
+      this.state.minLeft < 10 ? "0" + this.state.minLeft : this.state.minLeft;
+    let sec =
+      this.state.secLeft < 10 ? "0" + this.state.secLeft : this.state.secLeft;
+
+    return min + ":" + sec;
+  }
+
+  getTime(timeLeft) {
+    if (timeLeft.type === "S") {
+      this.setState({
+        minLeft: timeLeft.time
+      });
+    }
   }
 
   render() {
@@ -76,13 +124,15 @@ class PomodoroComp extends React.Component {
           text={"Break Length"}
           valueID={"break-length"}
           value={5}
+          type={"B"}
+          getTime={this.getTime}
           btn1ID={"break-increment"}
           btn2ID={"break-decrement"}
           btn1Text={"+inc"}
           btn2Text={"-dec"}
         />
 
-        <DisplayComp timeLeft={"00:00"} />
+        <DisplayComp timeLeft={this.timeleftString()} />
 
         <ControlComp
           controlClass={"SessionControl"}
@@ -90,6 +140,8 @@ class PomodoroComp extends React.Component {
           text={"Session Length"}
           valueID={"session-length"}
           value={25}
+          type={"S"}
+          getTime={this.getTime}
           btn1ID={"session-increment"}
           btn2ID={"session-decrement"}
           btn1Text={"+inc"}
