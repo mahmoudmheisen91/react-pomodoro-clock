9bb2a392cd4eae88ded9acbb2c0cc72a9cdd3595
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-04 01:47:32 +0300

    Updates

---
+++ precache-manifest.8fa5f31cfffc9a680c57068f62245b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a4e694e75842348a0633986323d551",
+    "url": "/react-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "1d635a2d215081501817",
+    "url": "/react-pomodoro-clock/static/css/main.b3b75ab1.chunk.css"
+  },
+  {
+    "revision": "58cc75d9943a5aad2063",
+    "url": "/react-pomodoro-clock/static/js/2.a63f8a65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro-clock/static/js/2.a63f8a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d635a2d215081501817",
+    "url": "/react-pomodoro-clock/static/js/main.d7ce9b07.chunk.js"
+  },
+  {
+    "revision": "cac0147741727ab5b0e6",
+    "url": "/react-pomodoro-clock/static/js/runtime-main.f8a04880.js"
+  }
+]);
