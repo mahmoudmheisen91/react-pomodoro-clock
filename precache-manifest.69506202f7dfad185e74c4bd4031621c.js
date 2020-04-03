b6a8dd5e73984d5748162f58356cda9827797011
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-04 02:35:35 +0300

    Updates

--- precache-manifest.69506202f7dfad185e74c4bd4031621c.js
+++ precache-manifest.69506202f7dfad185e74c4bd4031621c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d24cfcd4b25b3746cf28723bbd4e120",
+    "revision": "50ca30e3daa910156c378d4d5b5b038c",
     "url": "/react-pomodoro-clock/index.html"
   },
   {
-    "revision": "d1354f25f32902983c74",
+    "revision": "db8d6edc93f0decfec1c",
     "url": "/react-pomodoro-clock/static/css/main.b3b75ab1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-pomodoro-clock/static/js/2.a63f8a65.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d1354f25f32902983c74",
-    "url": "/react-pomodoro-clock/static/js/main.e98a0491.chunk.js"
+    "revision": "db8d6edc93f0decfec1c",
+    "url": "/react-pomodoro-clock/static/js/main.3992356f.chunk.js"
   },
   {
     "revision": "cac0147741727ab5b0e6",
